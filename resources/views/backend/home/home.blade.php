eb4ef802a783982963ed0b688ceea665af9ab88d
Nadim Hossain <nadimhossain6969@gmail.com>
2023-10-04 00:11:38 +0600

    add

--- resources/views/backend/home/home.blade.php
+++ resources/views/backend/home/home.blade.php
@@ -14,6 +14,7 @@
                 <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-3">
                     Main Dashboard
                 </h1>
+                <h2>Nadim</h2>
 {{--                <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">--}}
 {{--                    <li class="breadcrumb-item text-muted">--}}
 {{--                        <a href="" class="text-muted text-hover-primary">Dashboard</a>--}}
